b383beea55e29fb07965f45999bd851c1ea111cb
Muskan <muskanyav06132013@gmail.com>
2025-05-16 10:51:42 +0530

    Update task1_factorial.py

--- task1_factorial.py
+++ task1_factorial.py
@@ -1,10 +1,10 @@
 #calculate factorial 
-def factorial(num,fact):
-    if num==0 or num ==1:
-        return fact
-    fact *= num
-    return factorial(num-1,fact)
+def factorial(num):
+    if num ==0 or num==1:
+        return 1
+   
+    return num * factorial(num-1)
     
 num = int(input("Enter the number: "))
-result = factorial(num,1)
+result = factorial(num)
 print("Factorial of",num,"is:",result)
